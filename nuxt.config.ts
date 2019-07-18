42550a82944d4f3094b881ce630f7c8b615e1203
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-18 11:46:46 +0200

    change favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,37 @@ const config: NuxtConfiguration = {
    ** Headers of the page
    */
   head: {
-    title: 'Vue Router Path Rank tester',
+    title: 'Vue Router Path Ranker',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'A Vue Router path rank tester',
+        content: 'A Vue Router path ranker',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: 'site.webmanifest' },
+    ],
   },
   /*
    ** Customize the progress-bar color
