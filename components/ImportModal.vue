25ef16221f43f5d4b33bb2cd5c72d6eb9bd11a6e
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-22 13:25:45 +0200

    do not open the modal by default

--- components/ImportModal.vue
+++ components/ImportModal.vue
@@ -149,11 +149,6 @@ export default class ImportModal extends Vue {
       this.error = error
     }
   }
-
-  mounted() {
-    // during testing
-    this.open()
-  }
 }
 
 function addRouteToPaths(
