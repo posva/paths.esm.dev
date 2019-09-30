5a1dfcedd9bb9915b19040dbf731e5fbc1bb8cea
Eduardo San Martin Morote <posva13@gmail.com>
2019-09-30 17:37:18 +0200

    fix: root calculation

--- api/path-rank.ts
+++ api/path-rank.ts
@@ -25,8 +25,8 @@ const enum PathScore {
   Wildcard = -1 * _multiplier, // /:namedWildcard(.*)
   SubWildcard = 1 * _multiplier, // Wildcard as a subsegment
   Repeatable = -0.5 * _multiplier, // /:w+ or /:w*
-  Strict = 0.7 * _multiplier, // when options strict: true is passed, as the regex omits \/?
-  CaseSensitive = 0.25 * _multiplier, // when options strict: true is passed, as the regex omits \/?
+  Strict = 0.07 * _multiplier, // when options strict: true is passed, as the regex omits \/?
+  CaseSensitive = 0.025 * _multiplier, // when options strict: true is passed, as the regex omits \/?
   Optional = -4 * _multiplier, // /:w? or /:w*
   SubOptional = -0.1 * _multiplier, // optional inside a subsegment /a-:w? or /a-:w*
   Root = 1 * _multiplier, // just /
@@ -54,7 +54,7 @@ export function createRouteMatcher(
 
   // special case for root path
   if (tokens.length === 1 && tokens[0] === '/') {
-    score = 5
+    score = (PathScore.Segment + PathScore.Root) * PathScore._multiplier
   } else {
     // allows us to group tokens into one single segment
     // it will point to the first token of the current group
